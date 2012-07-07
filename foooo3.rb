316621d2c6941598a911f5dfa0f3358fbbd7a976
USAMI Kenta <tadsan@zonu.me>
2012-07-08 02:28:27 +0900

    switch direct exec and required

--- foooo3.rb
+++ foooo3.rb
@@ -1,18 +1,46 @@
 #!/usr/bin/env ruby
 require 'strscan'
 
-code = open(ARGV[0]){|f| f.read }
-
-s = StringScanner.new(code)
-n = 0
-
-while ! s.eos?
-  case
-    when (s.scan /^(fo*)\n/)
-      n += 1
-    else
-      raise
+module FooooScript
+  OUTPUT_STRING = 'foooo'
+  RE_VALID_LINE = /^(fo*)\n/
+  class Scanner < StringScanner; end
+  class Interpreter
+    def self.open(path)
+      raise unless File.exist? path
+      code = Kernel.open(path, 'r'){|r| r.read}
+      new code
+    end
+    def initialize(str)
+      @n = 0
+      @scanner = Scanner.new(str)
+    end
+    def run
+      (@parsed || parse) && out
+    end
+    def out
+      OUTPUT_STRING * @n
+    end
+    def parse
+      s = @scanner
+      while ! s.eos?
+        case
+        when (s.scan RE_VALID_LINE)
+          @n += 1
+        else
+          raise
+        end
+      end
+      @parsed = true
+    end
   end
 end
 
-puts 'foooo' * n
+def FooooScript(path)
+  FooooScript::Interpreter.open path
+end
+
+if $0 == __FILE__
+    puts FooooScript(ARGV[0]).run
+end
+
