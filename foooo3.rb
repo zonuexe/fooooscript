897ca901165c5470f6a8082ec9733f4cab809e20
USAMI Kenta <tadsan@zonu.me>
2012-07-08 01:07:57 +0900

    add ruby objective interpriter

---
+++ foooo3.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require 'strscan'
+
+code = open(ARGV[0]){|f| f.read }
+
+s = StringScanner.new(code)
+n = 0
+
+while ! s.eos?
+  case
+    when (s.scan /^(fo*)\n/)
+      n += 1
+    else
+      raise
+  end
+end
+
+puts 'foooo' * n
